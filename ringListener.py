e508591f29fdbc5e874dd5f8db28b60fc041d8df
Robert <robert@bigwings.de>
2018-01-17 22:05:32 +0100

    moved value

--- ringListener.py
+++ ringListener.py
@@ -11,7 +11,7 @@ def startListeningForRing(tokenhandler):
         GPIO.setup(gpio_pin, GPIO.IN)
         GPIO.add_event_detect(gpio_pin, GPIO.FALLING, callback = signaler.sendNotification, bouncetime = 800)
         while True:
-            print("Listening for ring on GPIO PIN #" + str(gpio_pin)
+            print("Listening for ring on GPIO PIN #" + str(gpio_pin))
             sleep(0.2)
         print("Stopped listing for Ring")
     except KeyboardInterrupt:
