17dcd99b3999284a259a3c9ee2d137c6d05a57fb
Robert <robert@bigwings.de>
2018-01-17 19:57:49 +0100

    basic setup

--- ringListener.py
+++ ringListener.py
@@ -8,10 +8,12 @@ def startListeningForRing(tokenhandler):
     try:
         signaler = RingSignaler(tokenhandler)
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(gpio_pin, GPIO.IN)
         GPIO.add_event_detect(gpio_pin, GPIO.FALLING, callback = signaler.sendNotification, bouncetime = 400)
         while True:
-            sleep(1)
+            print(.)
+            sleep(0.1)
+            print(..)
     except KeyboardInterrupt:
         GPIO.cleanup()
         print("stops listening for ring")
