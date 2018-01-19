e6bc27a9a9c9c85c50022ee7a6f5b1d78e4366dc
Robert <robert@bigwings.de>
2018-01-19 19:53:25 +0100

    yolo

--- doorOpener.py
+++ doorOpener.py
@@ -7,9 +7,9 @@ strikeDuration = 1
 def openDoor():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(gpio_pin, GPIO.OUT)
-    GPIO.output(gpio_pin, 0)
+    GPIO.output(gpio_pin, 1)
     print("Opening door for " + str(strikeDuration) + " seconds.")
     sleep(strikeDuration)
-    GPIO.output(gpio_pin, 0)
+    GPIO.output(gpio_pin, 1)
 
 openDoor()
