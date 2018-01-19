fc6a13e88b9f090002a0c22f81d000fa46a9956e
Robert <robert@bigwings.de>
2018-01-19 21:37:56 +0100

    yolo

--- messageListener.py
+++ messageListener.py
@@ -1,13 +1,17 @@
 import secrets
 import telegram
 import doorOpener
-from telegram.ext import Updater, CallbackQueryHandler
+from telegram.ext import Updater, CallbackQueryHandler, CommandHandler
 
 def handleButtonCallback(bot, update):
     msg = update.callback_query.data
     if (msg == "open"):
         bot.sendMessage(secrets.chatId, text="Tür wird geöffnet")
         doorOpener.openDoor()
+
+def handleOpenCommand(bot, update):
+    bot.sendMessage(secrets.chatId, text="Tür wird geöffnet")
+    doorOpener.openDoor()
 
 def messageListener():
     print("Starting Telegram message listener...")
@@ -16,6 +20,9 @@ def messageListener():
 
     callback_handler = CallbackQueryHandler(handleButtonCallback)
 
+    command_handler = CommandHandler("open", handleOpenCommand)
+
     dispatcher.add_handler(callback_handler)
+    dispatcher.add_handler(command_handler)
 
     updater.start_polling()
