a4aeabfe46a2fd0e9546dd9bd9716360b477233c
Robert <robert@bigwings.de>
2018-01-20 10:23:43 +0100

    yolo

--- messageListener.py
+++ messageListener.py
@@ -3,21 +3,28 @@ import telegram
 import doorOpener
 from telegram.ext import Updater, CallbackQueryHandler, CommandHandler
 
+def isAuthorized(chatId):
+    return if chatId in secrets.validChats
+    print("Calling chatId isn't valid.")
+
 def handleButtonCallback(bot, update):
-    msg = update.callback_query.data
-    if (msg == "open"):
-        print("Received callback instruction to open the door.")
+    if(isAuthorized(update))
+        msg = update.callback_query.data
+        if (msg == "open"):
+            print("Received callback instruction to open the door.")
+            bot.sendMessage(secrets.chatId, text="Tür wird geöffnet")
+            doorOpener.openDoor()
+
+def handleOpenCommand(bot, update):
+    if(isAuthorized(update))
+        print("Received message instruction to open the door.")
         bot.sendMessage(secrets.chatId, text="Tür wird geöffnet")
         doorOpener.openDoor()
 
-def handleOpenCommand(bot, update):
-    print("Received message instruction to open the door.")
-    bot.sendMessage(secrets.chatId, text="Tür wird geöffnet")
-    doorOpener.openDoor()
-
 def handleStartCommand(bot, update):
-    print("Received start command.")
-    bot.sendMessage(secrets.chatId, text="Hi! Send /open to open the door or wait for ring signal.")
+    if(isAuthorized(update))
+        print("Received start command.")
+        bot.sendMessage(secrets.chatId, text="Hi! Send /open to open the door or wait for ring signal.")
 
 def messageListener():
     print("Starting Telegram message listener...")
