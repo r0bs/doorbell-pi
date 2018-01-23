8e4046b9450037c92f515fd5c427aca70cd754e4
Robert <robert@bigwings.de>
2018-01-23 22:22:14 +0100

    #1 doorbell systemd service file

--- ringListener.py
+++ ringListener.py
@@ -17,6 +17,6 @@ def startListeningForRing():
 
 def ringHandler(pin):
     localtime = time.asctime( time.localtime(time.time()) )
-    print("Doorbell" + pin +" rang at: " + localtime)
+    print("Doorbell" + str(pin) +" rang at: " + localtime)
 
     notifier.sendNotification()
