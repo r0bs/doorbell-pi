49c591413edc1a015b8f6db13b1330b14f79e581
Robert <robert@bigwings.de>
2018-01-23 23:06:03 +0100

    added todos to installer

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# WIP
+# TODO: Add libraries used
+# TODO: Test on blank system
+
 sudo cp /home/pi/doorbell-pi/doorbell.service /lib/systemd/system/doorbell.service
 sudo chmod 644 /lib/systemd/system/doorbell.service
 sudo systemctl daemon-reload
-sudo systemctl enable myscript.service
-sudo systemctl start myscript.service
-sudo systemctl status myscript.service
+sudo systemctl enable doorbell.service
+sudo systemctl start doorbell.service
+sudo systemctl status doorbell.service
