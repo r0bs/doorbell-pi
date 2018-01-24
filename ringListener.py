c0908648dbee1494e9f6e73f046aca32ee1221fb
Robert <robert@bigwings.de>
2018-01-24 23:57:16 +0100

    #10 testing workaround

--- ringListener.py
+++ ringListener.py
@@ -7,7 +7,7 @@ import gpioConfig
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def startListeningForRing():
-    GPIO.add_event_detect(gpioConfig.ringListenerPin, GPIO.BOTH, callback = ringHandler, bouncetime=20)
+    GPIO.add_event_detect(gpioConfig.ringListenerPin, GPIO.BOTH, callback = ringHandler)
     print("Listening for ring signal on GPIO PIN #" + str(gpioConfig.ringListenerPin))
     
     while True:
@@ -16,9 +16,10 @@ def startListeningForRing():
 
 
 def ringHandler(pin):
-    global starttime
-    global endtime
-    if GPIO.input(gpioConfig.ringListenerPin) == 0:
+    if "starttime" not in globals():
+        global starttime = False
+        
+    if GPIO.input(gpioConfig.ringListenerPin) == 0 and starttime is False:
         starttime = int(round(time.time() * 1000))
     elif starttime is not False:
         endtime = int(round(time.time() * 1000))
