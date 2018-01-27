a39af6b4afdb7ee2bcf122ca8120c9001399240b
Robert <robert@bigwings.de>
2018-01-27 14:18:25 +0100

    #12 added first buzzer implementation

--- buzzer.py
+++ buzzer.py
@@ -4,9 +4,9 @@ from time import sleep
 
 def buzz():
     print("Making sound")
-    for beep in range(0, 10):
+    for beep in range(0, 4):
         GPIO.output(gpioConfig.buzzerPin, 1)
-        sleep(0.1)
+        sleep(0.2)
         GPIO.output(gpioConfig.buzzerPin, 0)
         sleep(0.3)
     print("Sound stopped.")
