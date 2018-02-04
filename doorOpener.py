c996aeb5cf30371042b937f1f86d8f554dd9f5e2
Robert <robert@bigwings.de>
2018-02-04 12:41:59 +0100

    new relay board, changed configuration

--- doorOpener.py
+++ doorOpener.py
@@ -3,10 +3,10 @@ import gpioConfig
 from time import sleep
 
 def openDoor():
-    GPIO.output(gpioConfig.doorOpenerPin, 1)
+    GPIO.output(gpioConfig.doorOpenerPin, 0)
     print("Opening door for " + str(gpioConfig.strikeDuration) + " seconds.")
     print("...")
     sleep(gpioConfig.strikeDuration)
     print("Opening door done.")
-    GPIO.output(gpioConfig.doorOpenerPin, 0)
+    GPIO.output(gpioConfig.doorOpenerPin, 1)
     
